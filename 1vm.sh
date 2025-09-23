79a9ba1b1f071a75169ec9ebc71f00b91568a31d
Gowtham Kumar <gowthmks1010@gmail.com>
2025-09-23 13:19:41 +0530

    added another line in 1vm.sh

--- 1vm.sh
+++ 1vm.sh
@@ -4,6 +4,5 @@ echo "Enter public ip"
 echo "Enter number with dash, ex- 15-2-222-222 like this"
 read -rp "IP: " ip && ssh -i "office.pem" ubuntu@ec2-$ip.ap-south-1.compute.amazonaws.com
 
-
-
 echo "aajhvdgsnhdhgvhjd"
+echo "jjjbgvhvefefef"
