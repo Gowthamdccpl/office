7929f756290808cc60bc8c30e3326a503f505815
Gowtham Kumar <gowthmks1010@gmail.com>
2025-09-23 10:49:52 +0530

    original

--- bash1.sh
+++ bash1.sh
@@ -4,13 +4,12 @@ echo "=== EC2 Login ==="
 read -rp "Enter Public IP: " PUBLIC_IP                   # asking for public ip with user
 PUBLIC_IP=$(echo "$PUBLIC_IP" | sed 's/\./-/g')          # converting the dots into dash      
 
-if [[ "$PUBLIC_IP" =~ ^[0-9]+$ ]]; then
+if [[ "$PUBLIC_IP" =~ ^[0-9.-]+$ ]]; then
     echo "Numeric"
-elif [[ "$PUBLIC_IP" =~ ^[a-zA-Z0-9]+$ ]]; then
-    echo "Alphanumeric"
 else
-    echo "Not alphanumeric"
+    echo "Alphanumeric"
 fi
+
                                                  
 HOSTNAME="ec2-${PUBLIC_IP}.ap-south-1.compute.amazonaws.com"   # Building hostname to connect
 
