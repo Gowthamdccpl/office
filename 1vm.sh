0779ee8dbc5b126aa6de551a3ea21538920175ba
Gowtham Kumar <gowthmks1010@gmail.com>
2025-09-23 13:23:54 +0530

    added2 lines

--- 1vm.sh
+++ 1vm.sh
@@ -3,3 +3,5 @@
 echo "Enter public ip"
 echo "Enter number with dash, ex- 15-2-222-222 like this"
 read -rp "IP: " ip && ssh -i "office.pem" ubuntu@ec2-$ip.ap-south-1.compute.amazonaws.com
+echo "thanks for using this script"
+echo "thyanks"
